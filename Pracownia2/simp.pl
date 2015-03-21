adb00a8768805d29f711f38eb87a2afc94794c46
scoiatael <czapl.luk@gmail.com>
2015-03-21 21:32:52 +0000

    more predictable

--- Pracownia2/simp.pl
+++ Pracownia2/simp.pl
@@ -4,25 +4,30 @@ simp(St, Y) :- prepare(St, X), findall(Z, simplify(X, Z), B), find_best(B, Best)
 
 prepare(X, Y) :- elim_minus(X, Z, 1), push_to_common_root(Z, Y), !.
 
+pipe([], X, X).
+pipe([H|T], Acc, X) :- F =.. [ H, Acc, P], F, pipe(T, P, X).
+
+map(_, [], []) :- !.
+map(F, [H|T], [Hp | Tp]) :- Fun =.. [F, H, Hp], Fun, map(F, T, Tp).
+
 simplify(X, Z) :-
-  push_to_common_root(X, Xp),
-  sort_tree(Xp, Xpp),
-  cluster(Xpp, Xppp),
-  elim_zeroes(Xppp, Xpppp),
-  distribute(Xpppp, Y),
+  pipe([
+    push_to_common_root,
+    sort_tree,
+    cluster,
+    elim_zeroes,
+    distribute
+  ], X, Y),
   loop_simplify(X, Y, Z).
 
 loop_simplify(X, Y, Z) :- \+ X=Y, !, simplify(Y, Z).
 loop_simplify(X, X, X) :- !.
 
 elim_zeroes(F, F) :- atomic(F), !.
-elim_zeroes(F, Fp) :- F =.. [Op | Args], map_elim_zeroes(Args, X),
+elim_zeroes(F, Fp) :- F =.. [Op | Args], map(elim_zeroes, Args, X),
   ( Op = (*), (member(0, X), !, Xp = [0] ; Xp = X)
   ; Op = (+), filter_zeroes(X, Xp)
   ), elim_singleton_op(Op, Xp, Fp), !.
-
-map_elim_zeroes([], []) :- !.
-map_elim_zeroes([H|T], [Hp|Tp]) :- elim_zeroes(H, Hp), map_elim_zeroes(T, Tp).
 
 filter_zeroes([], []) :- !.
 filter_zeroes([0|T], Tp) :- !, filter_zeroes(T, Tp).
@@ -36,10 +41,7 @@ elim_minus(F, Fp, V) :- F =.. [(+), A, B], elim_minus(A, Ap, V), elim_minus(B, B
 elim_minus(F, Fp, V) :- F =.. [(*), A, B], elim_minus(A, Ap, V), elim_minus(B, Bp, 1), Fp =.. [(*), Ap, Bp].
 
 push_to_common_root(X, X):- atomic(X), !.
-push_to_common_root(Z, F):- Z =.. [Op | Li], map_push_to_common_root(Li, Lip), fold_append_by_op(Op, Lip, F).
-
-map_push_to_common_root([], []).
-map_push_to_common_root([H|T], [Hp|Tp]) :- push_to_common_root(H, Hp), map_push_to_common_root(T, Tp).
+push_to_common_root(Z, F):- Z =.. [Op | Li], map(push_to_common_root, Li, Lip), fold_append_by_op(Op, Lip, F).
 
 append_by_op(Op, A, B, F) :- A =.. [Op | Ap], B =.. [Op | Bp], !, append(Ap, Bp, Fp), F =.. [Op | Fp].
 append_by_op(Op, A, B, F) :- A =.. [Op | Ap], !, F =.. [Op, B | Ap].
@@ -50,20 +52,14 @@ fold_append_by_op(_, [H], H) :- !.
 fold_append_by_op(Op, [H|T], Ff) :- fold_append_by_op(Op, T, Tp), append_by_op(Op, H, Tp, Ff).
 
 sort_tree(X, X) :- atomic(X), !.
-sort_tree(F, Fp) :- F =.. [Op | Li], map_sort_tree(Li, Lip), !, msort(Lip, Lipp), Fp =.. [Op | Lipp].
-
-map_sort_tree([], []).
-map_sort_tree([X|Y], [Xp|Yp]) :- sort_tree(X, Xp), map_sort_tree(Y, Yp).
+sort_tree(F, Fp) :- F =.. [Op | Li], map(sort_tree, Li, Lip), !, msort(Lip, Lipp), Fp =.. [Op | Lipp].
 
 cluster(X, X) :- atomic(X), !.
 cluster(F, Fp) :-
   F =.. [Op | Li],
-  map_cluster(Li, Lip),
+  map(cluster, Li, Lip),
   fold_cluster(Op, Lip, Lipp),
   elim_singleton_op(Op, Lipp, Fp), !.
-
-map_cluster([], []).
-map_cluster([X|Y], [Xp|Yp]) :- cluster(X, Xp), map_cluster(Y, Yp).
 
 fold_cluster((-), X, F) :- cluster_num((-), X, Xp, Y), F = [ Xp | Y].
 fold_cluster((*), X, F) :- cluster_num((*), X, Xp, Y), crunch_factors(1, Xp, Y, F).
@@ -82,7 +78,6 @@ eval_list(Op, [H|T], S) :- eval_list(Op, T, Sp), Ev =.. [Op, Sp, H], S is Ev.
 eval_list((-), H, [], H).
 eval_list((-), X, [H|T], S) :- Sp is X - H, eval_list((-), Sp, T, S).
 
-
 take_numbers([X|Y], [X|Yp], Z) :- number(X), !, take_numbers(Y, Yp, Z).
 take_numbers(Y, [], Y).
 
@@ -95,19 +90,17 @@ count(_, Y, 0, Y).
 distribute(X, X) :- atomic(X), !.
 distribute(F, Fp) :-
   F =.. [Op | Li],
-  map_distribute(Li, Lip),
+  map(distribute, Li, Lip),
   (member(Op, [(+)]),
     group_by_common_factor(Lip, Lipp),
     \+ Lip = Lipp,
+    !,
     elim_singleton_op(Op, Lipp,  Fp)
   ;
     Fp =.. [Op | Lip]).
 
 elim_singleton_op(_, [Fpp], Fpp):- !.
 elim_singleton_op(Op, Lipp, Fpp) :- Fpp =.. [Op | Lipp].
-
-map_distribute([], []).
-map_distribute([H|T], [Hp|Tp]) :- distribute(H, Hp), map_distribute(T, Tp).
 
 group_by_common_factor([], []).
 group_by_common_factor([H|T], [Lpp | D]) :-
@@ -133,22 +126,18 @@ elim_singleton(R, R).
 find_common(_, T, [], T).
 find_common(A, [H|T], [Rp|C], D) :- H =.. [(*) | Args], select(A, Args, R), elim_singleton_op((*), R, Rp), find_common(A, T, C, D).
 
-find_best(Opts, Best) :- map_term_size(Opts, Optsp), find_shortest(Optsp, _-Best).
+find_best(Opts, Best) :- map(term_size, Opts, Optsp), find_shortest(Optsp, _-Best).
 
-term_size(X, 1) :- atomic(X),!.
-term_size(F, T) :- F =.. [_ | Li], reduce_term_size(Li, S), sum_list([1 | S], T).
+term_size(X, 1-X) :- atomic(X),!.
+term_size(F, T-F) :- F =.. [_ | Li], reduce_term_size(Li, S), sum_list([1 | S], T).
 
 reduce_term_size([], []).
 reduce_term_size([H|T], [S | Ts]) :- term_size(H, S), reduce_term_size(T, Ts).
 
-map_term_size([], []).
-map_term_size([H|T], [S-H|Tp]) :- term_size(H, S), map_term_size(T, Tp).
-
 find_shortest(T, Z) :- keysort(T, [Z | _]).
 
 convert_back(M, M) :- atomic(M), !.
-convert_back(M, S) :- M =.. [Op | Li], map_convert_back(Li, Lip), group_with(Op, Lip, S), !.
-map_convert_back([], []).
-map_convert_back([H|T], [Hp|Tp]) :- convert_back(H, Hp), map_convert_back(T, Tp).
+convert_back(M, S) :- M =.. [Op | Li], map(convert_back, Li, Lip), group_with(Op, Lip, S), !.
+
 group_with(_, [A], A).
 group_with(Op, [H|T], F) :- group_with(Op, T, Fp), F =.. [Op, H, Fp].
