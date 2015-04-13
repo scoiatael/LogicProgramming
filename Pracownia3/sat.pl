91d69d0627ac46b0e9da00e991ceb63179d319d1
scoiatael <czapl.luk@gmail.com>
2015-04-13 19:49:37 +0000

    sat fix

--- Pracownia3/sat.pl
+++ Pracownia3/sat.pl
@@ -4,5 +4,5 @@ sat(X) :- rewrite(X, P), term_variables(P, V), ins(V, 0 \/ 1), P #= 1, label(V).
 
 rewrite(X, X) :- var(X), !.
 rewrite(and(X, Y), XP*YP) :- !, rewrite(X, XP), rewrite(Y, YP).
-rewrite(or(X, Y), XP+YP) :- !, rewrite(X, XP), rewrite(Y, YP).
+rewrite(or(X, Y), max(XP,YP)) :- !, rewrite(X, XP), rewrite(Y, YP).
 rewrite(neg(X), (1-XP)) :- !, rewrite(X, XP).
