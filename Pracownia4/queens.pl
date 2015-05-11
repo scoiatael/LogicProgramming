4f21782528f754babc3391ad71d6285331c8bf7b
scoiatael <eternal.kadir@gmail.com>
2015-05-11 19:56:18 +0200

    proper name for queens

--- Pracownia4/queens.pl
+++ Pracownia4/queens.pl
@@ -1,22 +1,26 @@
 :- use_module(library(clpfd)).
 
-nqueens(N, Pairs) :- n_vars(N, Pairs, Xs, Ys),
+queens(N, Pairs) :- n_vars(N, Pairs, Xs, Ys),
   all_distinct(Xs),
   all_distinct(Ys),
   append(Xs, Ys, Vars),
   Vars ins 1..N,
   no_diagonal_checks(Pairs),
-  label(Pairs).
+  monotonic(Xs),
+  label(Vars).
 
 no_diagonal_checks(Pairs) :- no_diagonal_checks_left_down(Pairs), no_diagonal_checks_left_up(Pairs).
 diff((X,Y), Z) :- Z #= X-Y.
 sum((X,Y), Z) :- Z #= X+Y.
 
-no_diagonal_checks_left_up(Pairs) :- maplist(Pairs, diff, Zs), all_distinct(Zs).
-no_diagonal_checks_left_down(Pairs) :- maplist(Pairs, sum, Zs), all_distinct(Zs).
+no_diagonal_checks_left_up(Pairs) :- maplist(diff, Pairs, Zs), all_distinct(Zs).
+no_diagonal_checks_left_down(Pairs) :- maplist(sum, Pairs, Zs), all_distinct(Zs).
 
-n_vars(0, [], [], []).
+n_vars(0, [], [], []) :- !.
 n_vars(N, [(X,Y)|T], [X|Xs], [Y|Ys]) :- N0 is N-1, n_vars(N0, T, Xs, Ys).
+
+monotonic([_]) :- !.
+monotonic([X,Y|Z]) :- X #=< Y, monotonic([Y|Z]).
 
 % 1,1 2,1 3,1
 % 1,2 2,2 3,2
