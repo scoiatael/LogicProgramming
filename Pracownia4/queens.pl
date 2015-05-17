b3da5b29f280533e24a9b06092596ffa267e55c4
scoiatael <eternal.kadir@gmail.com>
2015-05-17 20:38:35 +0200

    queens done

--- Pracownia4/queens.pl
+++ Pracownia4/queens.pl
@@ -1,26 +1,23 @@
 :- use_module(library(clpfd)).
 
-queens(N, Pairs) :- n_vars(N, Pairs, Xs, Ys),
+queens(N, Xs) :- length(Xs, N),
+  add_Ys(0, Xs, Pairs),
+  Xs ins 1..N,
   all_distinct(Xs),
-  all_distinct(Ys),
-  append(Xs, Ys, Vars),
-  Vars ins 1..N,
   no_diagonal_checks(Pairs),
-  monotonic(Xs),
-  label(Vars).
+  labeling([ff, bisect], Xs).
 
-no_diagonal_checks(Pairs) :- no_diagonal_checks_left_down(Pairs), no_diagonal_checks_left_up(Pairs).
-diff((X,Y), Z) :- Z #= X-Y.
-sum((X,Y), Z) :- Z #= X+Y.
+no_diagonal_checks([_]) :- !.
+no_diagonal_checks([Q|Qs]) :- no_diagonal_checks_left_down(Q, Qs), no_diagonal_checks_left_up(Q, Qs), no_diagonal_checks(Qs).
 
-no_diagonal_checks_left_up(Pairs) :- maplist(diff, Pairs, Zs), all_distinct(Zs).
-no_diagonal_checks_left_down(Pairs) :- maplist(sum, Pairs, Zs), all_distinct(Zs).
+no_diagonal_checks_left_up(Q, Qs) :- maplist(diff(Q), Qs).
+no_diagonal_checks_left_down(Q, Qs) :- maplist(sum(Q), Qs).
 
-n_vars(0, [], [], []) :- !.
-n_vars(N, [(X,Y)|T], [X|Xs], [Y|Ys]) :- N0 is N-1, n_vars(N0, T, Xs, Ys).
+diff((X,Y), (A,B)) :- X-Y #\= A-B.
+sum((X,Y), (A,B)) :- X+Y #\= A+B.
 
-monotonic([_]) :- !.
-monotonic([X,Y|Z]) :- X #=< Y, monotonic([Y|Z]).
+add_Ys(_, [], []) :- !.
+add_Ys(N, [X|Xs], [(X,N)|Pairs]) :- N0 is N+1, add_Ys(N0, Xs, Pairs).
 
 % 1,1 2,1 3,1
 % 1,2 2,2 3,2
