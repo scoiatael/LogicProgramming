8dcbc78be7b80ccb1a23b7b56b6ef52411add308
scoiatael <czapl.luk@gmail.com>
2015-05-18 16:41:57 +0000

    osemka done

--- Pracownia4/osemka.pl
+++ Pracownia4/osemka.pl
@@ -13,8 +13,8 @@ f_value(X, Depth, E, Value) :-
 search([(_, (End,_,Sol)) | _], End, Sol).
 search([(_, State)|R], End, Solution) :-
   State = (S, _, _),
-  ( fail, was_here(S), !, Next = R
-  ; write_state(State),
+  ( was_here(S), !, Next = R
+  ;
     expand(State, End, States),
     assert(was_here(S)),
     append(States, R, Rest),
@@ -22,28 +22,20 @@ search([(_, State)|R], End, Solution) :-
   ),
   search(Next, End, Solution).
 
-write_state((S, _, _)) :- pretty_print(S), nl.
-pretty_print([A1, A2, A3, A4, A5, A6, A7, A8, A9]) :-
-  write([A1, A2, A3]), nl,
-  write([A4, A5, A6]), nl,
-  write([A7, A8, A9]), nl.
-
 expand((State,D,S), E, Nexts) :-
   D1 is D+1,
-  bagof((F, (C,D1,[Move|S])),
-  ( move(State, C, Move), % \+ was_here(C),
+  findall((F, (C,D1,[Move|S])),
+  ( move(State, C, Move), \+ was_here(C),
     f_value(C, D1, E, F)),
     Nexts
   ).
 
-osemka(Start, End, Num) :- solve(Start, End, S), !, length(S, Num).
+osemka(Start, End, Num) :- solve(End, Start, S), !, length(S, Num).
 
-h_value(_, _, 0).
-%h_value(Li, Final, N) :-
-%  %  Li = [A1,A2,A3,A4,A5,A6,A7,A8,A9],
-%  manhattan(Li, Final, M),
-%  out_of_order(Li, Final, O),
-%  N is M+3*O.
+h_value(Li, Final, N) :-
+  manhattan(Li, Final, M),
+  out_of_order(Li, Final, O),
+  N is M+3*O.
 
 number_vars([], _, []).
 number_vars([H|T], N, [(H,N)|T0]) :- N0 is N+1, number_vars(T, N0, T0).
@@ -74,25 +66,27 @@ out_of_order((X, Y), 0) :- member(o, [X,Y]), !.
 out_of_order((X, Y), 0) :- X is Y+1, !.
 out_of_order((_, _), 1).
 
-move(Before, After, move) :-
+move(Before, After, Move) :-
   pos(Before, Num), (
-    ( Num = 1, !, ( down(Before, After) ; right(Before, After)))
-  ; ( Num = 2, !, ( down(Before, After) ; left_or_right(Before, After)))
-  ; ( Num = 3, !, ( down(Before, After) ; left(Before, After)))
-  ; ( Num = 4, !, ( up_or_down(Before, After) ; right(Before, After)))
-  ; ( Num = 5, !, ( up_or_down(Before, After) ; left_or_right(Before, After)))
-  ; ( Num = 6, !, ( up_or_down(Before, After) ; left(Before, After)))
-  ; ( Num = 7, !, ( up(Before, After) ; right(Before, After)))
-  ; ( Num = 8, !, ( up(Before, After) ; left_or_right(Before, After)))
-  ; ( Num = 9, !, ( up(Before, After) ; left(Before, After)))
+    ( Num = 1, !, ( down(Before, After, Move) ; right(Before, After, Move)))
+  ; ( Num = 2, !, ( down(Before, After, Move) ; left_or_right(Before, After, Move)))
+  ; ( Num = 3, !, ( down(Before, After, Move) ; left(Before, After, Move)))
+  ; ( Num = 4, !, ( up_or_down(Before, After, Move) ; right(Before, After, Move)))
+  ; ( Num = 5, !, ( up_or_down(Before, After, Move) ; left_or_right(Before, After, Move)))
+  ; ( Num = 6, !, ( up_or_down(Before, After, Move) ; left(Before, After, Move)))
+  ; ( Num = 7, !, ( up(Before, After, Move) ; right(Before, After, Move)))
+  ; ( Num = 8, !, ( up(Before, After, Move) ; left_or_right(Before, After, Move)))
+  ; ( Num = 9, !, ( up(Before, After, Move) ; left(Before, After, Move)))
   ).
 
 pos(B, N) :- append(Beg, [o|_], B), length(Beg, N0), N is N0+1.
 
-left(B, A) :- append(Beg, [X, o|E], B), append(Beg, [o, X|E], A).
-right(B, A) :- append(Beg, [o, X|E], B), append(Beg, [X, o|E], A).
-left_or_right(B, A) :- left(B, A) ; right(B, A).
+left(B, A, left) :- append(Beg, [X, o|E], B), append(Beg, [o, X|E], A).
+right(B, A, right) :- append(Beg, [o, X|E], B), append(Beg, [X, o|E], A).
+left_or_right(B, A, Move) :- left(B, A, Move) ; right(B, A, Move).
 
-up(B, A) :- append(Beg, [A1, A2, A3, o | E], B), append(Beg, [o, A1, A2, A3 | E], A).
-down(B, A) :- append(Beg, [o, A1, A2, A3 | E], B), append(Beg, [A1, A2, A3, o | E], A).
-up_or_down(B, A) :- up(B, A) ; down(B, A).
+up(B, A, up) :- append(Beg, [A1, A2, A3, o | E], B), append(Beg, [o, A2, A3, A1 | E], A).
+down(B, A, down) :- append(Beg, [o, A1, A2, A3 | E], B), append(Beg, [A3, A1, A2, o | E], A).
+up_or_down(B, A, Move) :- up(B, A, Move) ; down(B, A, Move).
+
+test([o,4,2,1,8,7,3,6,5],10).
