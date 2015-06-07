0794433e7d5fe7c9e4b1a6ace981f01d528722f1
scoiatael <czapl.luk@gmail.com>
2015-06-07 21:27:47 +0000

    almost working gwiez

--- Pracownia5/gwiez.pl
+++ Pracownia5/gwiez.pl
@@ -1,19 +1,27 @@
+:- use_module(library(clpfd)).
+
 % n_max([1,1,3,4,5,2,1], 4) % [1,3,4,5]
 % n_max([1,4,1,5,2,6], 4) % [1,4,5,6]
 
-calc_n_max([H|T], X) :- n_max(T, H, X0), X is X0+1.
+calc_n_max([H|T], X) :- calc_n_max(T, H, X0), X is X0+1.
 calc_n_max([], _, 0).
-calc_n_max([H|T], Max, N) :- H =< Max, !, n_max(T, Max, N).
-calc_n_max([H|T], Max, N) :- H > Max, !, n_max(T, Max, N0), N is N0+1.
+calc_n_max([H|T], Max, N) :- H =< Max, !, calc_n_max(T, Max, N).
+calc_n_max([H|T], Max, N) :- H > Max, !, calc_n_max(T, H, N0), N is N0+1.
 
-permi([], [[]]).
+permi([], []).
 permi([H|T], S) :-
   permi(T, S0),
   select(H, S, S0).
 
 generate_n_max(N) :-
   findall(X, between(1, N, X), List),
-  findall(_, (permi(List, Perm), calc_n_max(Perm, X), assert(n_max(Perm, X))), _).
+  findall(_, (
+    permi(List, Perm),
+    calc_n_max(Perm, X),
+    reverse(Perm, RPerm),
+    calc_n_max(RPerm, Y),
+    assert(n_max(Perm, (X, Y)))
+  ), _).
 
 wiezowce(N, Rows, Cols, Sq) :-
   generate_n_max(N),
@@ -38,3 +46,5 @@ row_constraints(Sq, Rows) :-
 
 row_constraint((RowDesc, Row)) :-
   n_max(Row, RowDesc).
+
+test(8, [ (2, 4), (3, 2), (1, 4), (3, 1), (2, 4), (3, 3), (3, 2), (2, 3)], [ (2, 3), (3, 3), (4, 1), (1, 3), (2, 2), (3, 2), (4, 2), (3, 2)]).
